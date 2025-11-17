aef193465efe2213bcb01046ab2ec959433df073
SMDSFC2005 <sergiomartinezdelgado05@gmail.com>
2025-11-17 12:06:53 +0100

    Portafolio 100%

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-/* ============================================================
+/*
            CAMBIO DE TEMA (CLARO/OSCURO) con almacenamiento en localStorage
-           ============================================================ */
+         */
 const body = document.body;
 const botonTema = document.getElementById('boton-tema');
 
@@ -9,6 +9,7 @@ body.setAttribute('data-tema', savedTheme);
 botonTema.textContent = savedTheme === 'claro' ? 'Modo Oscuro' : 'Modo Claro';
 
 botonTema.addEventListener('click', () => {
+    
     const nuevo = body.getAttribute('data-tema') === 'claro' ? 'oscuro' : 'claro';
     body.setAttribute('data-tema', nuevo);
     localStorage.setItem('tema', nuevo);
@@ -16,10 +17,10 @@ botonTema.addEventListener('click', () => {
     botonTema.textContent = nuevo === 'claro' ? 'Modo Oscuro' : 'Modo Claro';
 });
 
-/* ============================================================
+/*
    SISTEMA DE TRADUCCIONES
    Usa data-t para reemplazar el texto dinámicamente
-   ============================================================ */
+    */
 const botonIdioma = document.getElementById('boton-idioma');
 
 const translations = {
@@ -29,7 +30,7 @@ const translations = {
         sobremi: "Sobre mí",
         contacto: "Contacto",
         subtitulo: "Desarrollador web en formación | Apasionado por crear soluciones digitales",
-        descripcion_inicio: "Estudiante de 2º de DAW...",
+        descripcion_inicio: "Soy estudiante de 2º de Desarrollo de Aplicaciones Web, apasionado por crear soluciones modernas y funcionales. Me interesa el desarrollo full-stack y disfruto trabajando tanto en la parte visual como en la lógica interna de las aplicaciones. Actualmente sigo formándome en Java, PHP, JavaScript y frameworks como Laravel y React, con el objetivo de convertirme en un gran desarrollador y preparado para afrontar nuevos retos.",
         ver_proyectos: "Ver Proyectos",
         ver_cv: "Ver CV",
         titulo_proyecto: "Videoclub",
@@ -87,9 +88,9 @@ botonIdioma.addEventListener("click", () => {
     botonIdioma.textContent = lang === "es" ? "EN" : "ES";
 });
 
-/* ============================================================
+/*
    MODAL PARA VER EL CV
-   ============================================================ */
+ */
 const modalOverlay = document.getElementById("modal");
 const btnOpen = document.getElementById("ver-cv-btn");
 const btnClose = document.getElementById("modal-close");
